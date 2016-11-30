6fb4c96f2f4211c62ba24af8942a9a24eddf3984
Alex Gould <alexgould17@gmail.com>
2016-11-29 23:04:44 -0800

    Added onload functions & moved the event handlers there

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,26 @@
+var points = document.getElementsByClassName('point');
 var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
   var revealPoint = function(i) {
     points[i].style.opacity = 1;
     points[i].style.transform = "scaleX(1) translateY(0)";
     points[i].style.msTransform = "scaleX(1) translateY(0)";
     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
   };
-  for(var i = 0; i < points.length; i++) {
-    revealPoint(i);
+  for(var j = 0; j < points.length; j++) {
+    revealPoint(j);
   }
 };
+
+window.onload = function () {
+  if (window.innerHeight > 950) {
+    animatePoints(points);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  alert('The window has loaded!');
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(points);   
+    }
+  });
+}
